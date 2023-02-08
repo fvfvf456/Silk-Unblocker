89e79966874768336d615ad3368da26e659b63c9
Ri0t <114663696+VaporRi0t@users.noreply.github.com>
2023-02-08 07:23:34 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+
+var express = require('express')
+var Unblocker = require('unblocker');
+
+// Create Express Server
+var app = express();
+
+// Create Unblocker Instance, and Configure Our Express Server to Use It
+var unblocker = new Unblocker({prefix: '/proxy/'});
+app.use(unblocker);
+
+
+// Launches Server on Port 8080
+app.listen(process.env.PORT || 8080).on('upgrade', unblocker.onUpgrade);
+console.log("Node Unblocker Server Running On Port:", process.env.PORT || 8080)
